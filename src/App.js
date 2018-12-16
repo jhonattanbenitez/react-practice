101138e28509d4b4fa8124dae8201b0b3e4faa31
jhonattanbenitez <jhonattan.benitez@hotmail.com>
2018-12-15 23:12:11 -0500

    pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
+import Radium from 'radium';
 import './App.css';
 
 class App extends Component {
@@ -41,7 +42,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
     let persons = null;
     if (this.state.showPersons) {
@@ -60,6 +65,10 @@ class App extends Component {
 
       );
       style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
     const classes = [];
     if(this.state.persons.length <= 2){
@@ -79,4 +88,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
